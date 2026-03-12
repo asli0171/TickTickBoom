f85bb36b7d0b40133676ff4e4d5fe39e48ca7afd
asli0171 <aslifarrah@gmail.com>
2026-03-12 23:19:09 +0100

    Initial Spring Boot setup

---
+++ src/main/java/dk/kinoxp/ticktickboom/TickTickBoomApplication.java
@@ -0,0 +1,13 @@
+package dk.kinoxp.ticktickboom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickTickBoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickTickBoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kinoxp/ticktickboom/TickTickBoomApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kinoxp.ticktickboom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickTickBoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
